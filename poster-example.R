0e9bd0ee1aa37074775a75ddf5b3a89a2b21aea9
Martin Petr <bodkan@users.noreply.github.com>
2021-06-01 14:04:00 +0200

    Update poster-example.R

--- poster-example.R
+++ poster-example.R
@@ -1,38 +1,62 @@
+# This is an exact copy of the script from our poster, with
+# several minor modifications (to make it run in a cloud binder
+# instance) or additionas (we have a chance to show a little bit
+# more there han we could in the poster). We have documented these
+# changes in comments below.
+
+# the package is already installed and we can simply load it
 library(slendr)
+
+# 1. spatial context ------------------------------------------------------
 
 map <- world(xrange = c(-15, 60), yrange = c(20, 65),
              landscape = "naturalearth", crs = "EPSG:3035")
 
+# 'plot' is our generic function able to plot all spatial objects
+# created by this package
+plot(map)
+
 # geographic regions used later to define population boundaries
-europe <- region(map = map, polygon = list(
+europe <- region("Europe", map = map, polygon = list(
     c(-8, 35), c(10, 38), c(20, 35), c(25, 35),
     c(32, 45), c(20, 58), c(-5, 60), c(-15, 50))
 )
-anatolia <- region(map = map, polygon = list(
+anatolia <- region("Anatolia", map = map, polygon = list(
   c(28, 35), c(40, 35), c(42, 40),
   c(30, 43), c(27, 40), c(25, 38))
 )
 
+plot(europe, anatolia)
+
+# 2. population dynamics --------------------------------------------------
+
+# in order to make it possible to run the whole simulation using the
+# restricted resources of the free cloud binder instance we dramatically
+# decreased the number of individuals in each population `N`
+
 ehg <- population( # Eastern hunter-gatherers
-  "EHG", time = 10000, N = 4000, map = map, remove = 7000,
+  "EHG", time = 10000, N = 1000, map = map, remove = 7000,
   polygon = list(c(26, 55), c(38, 53), c(48, 53), c(60, 53),
                  c(60, 60), c(48, 63), c(38, 63), c(26, 60))
 )
+plot(ehg)
 
 ana <- population( # Anatolian farmers
-  name = "ANA", time = 10000, N = 8000, remove = 4000,
+  name = "ANA", time = 10000, N = 1000, remove = 4000,
   map = map, polygon = anatolia
 ) %>%
   expand(by = 2500e3, start = 10000, end = 7000,
          snapshots = 10, polygon = join(europe, anatolia))
+plot(ana)
 
 eur <- population( # European population
-  name = "EUR", time = 10000, N = 5000,
+  name = "EUR", time = 10000, N = 1000,
   map = map, polygon = europe
 )
+plot(eur)
 
 yam <- population( # Yamnaya steppe population
-  name = "YAM", time = 7000, N = 3000, remove = 3000,
+  name = "YAM", time = 7000, N = 1000, remove = 3000,
   parent = ehg, polygon = list(
     c(26, 50), c(38, 49), c(48, 50),
     c(48, 56), c(38, 59), c(26, 56)
@@ -40,6 +64,10 @@ yam <- population( # Yamnaya steppe population
 ) %>%
   move(trajectory = c(15, 50), start = 5000, end = 3000,
        snapshots = 8)
+plot(yam)
+
+
+# 3. geneflow events ------------------------------------------------------
 
 geneflows <- list(
   geneflow(ana, yam, start = 6500, end = 6000, rate = 0.3),
@@ -47,16 +75,33 @@ geneflows <- list(
   geneflow(yam, eur, start = 4000, end = 3000, rate = 0.75)
 )
 
+# model compilation -------------------------------------------------------
+
 model <- compile(
   populations = list(ehg, ana, yam, eur), geneflows = geneflows,
   generation_time = 30, resolution = 10e3,
   competition_dist = 200e3, mate_dist = 200e3,
-  offspring_dist = 100e3, dir = "/tmp/test-model"
+  offspring_dist = 100e3, dir = "/tmp/test-model",
+  overwrite = TRUE
 )
 
+model # when printed, model objects present a brief summary
+
+# 5. model visualization --------------------------------------------------
+
+# open the shiny explorer (you might get a note about a website trying
+# to open a popup window)
 explore(model)
 
+# only show the embedded admixture graph
+graph(model)
+
+# 6. execution in SLiM ----------------------------------------------------
+
+# we modified the SLiM script to be able to run it in the binder cloud
 slim(
-  model, seq_length = 100e6, recomb_rate = 1e-8,
-  save_locations = TRUE, ts_recording = TRUE, method = "gui"
+  model, seq_length = 1, recomb_rate = 0,
+  save_locations = TRUE, # only record the locations of all individuals
+  method = "batch", # simulate using command-line SLiM (could be also "gui")
+  verbose = TRUE
 )
